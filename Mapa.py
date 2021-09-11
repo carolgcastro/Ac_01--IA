9c53a3e4e384d5cb02bb523ee8ea62fd5c45f9ae
carollgcastro <carollgab39@gmail.com>
2021-09-10 22:21:10 -0300

    localização e conexões de acordo com a grafo

--- Mapa.py
+++ Mapa.py
@@ -1,35 +1,30 @@
 #conexoes entre pontos
 conexoes = {}
 
-conexoes["Carregamento"] = {"Estoque Pronto"}
-conexoes["Home e Energia"] = {"Estacao 1", "Estacao 4", "Estoque Intermediario"}
-conexoes["Estoque Intermediario"] = {"Deposito Insumos 2", "Home e Energia", 
-									"Estoque Pronto", "Estacao 3"}
+conexoes["Casa"] = {"Santo Amaro"}
+conexoes["Santo Amaro"] = {"Casa", "Socorro"}
+conexoes["Socorro"] = {"Santo Amaro", "Pinheiros"}
 
-conexoes["Deposito Insumos 1"] = {"Estacao 1"}
-conexoes["Deposito Insumos 2"] = {"Estacao 1", "Estoque Intermediario" , "Estacao 2"}
+conexoes["Pinheiros"] = {"Socorro", "República", "Luz"}
+conexoes["República"] = {"Pinheiros", "Barra Funda"}
+conexoes["Luz"] = {"Pinheiros", "Sé"}
 
-conexoes["Estoque Pronto"] = {"Estacao 4", "Estoque Intermediario", "Carregamento"}
-
-conexoes["Estacao 1"] = {"Deposito Insumos 1", "Deposito Insumos 2","Home e Energia"}
-conexoes["Estacao 2"] = {"Deposito Insumos 2", "Estacao 3"}
-conexoes["Estacao 3"] = {"Estacao 2", "Estoque Intermediario"}
-conexoes["Estacao 4"] = {"Home e Energia"}
-#conexoes["Estacao 4"] = {"Home e Energia", "Estoque Pronto"}
+conexoes["Sé"] = {"Luz", "Barra Funda"}
+conexoes["Barra Funda"] = {"República", "Sé", "Impacta"}
+conexoes["Impacta"] = {"Barra Funda"}
 
 #localização de todos os lugares 
 
 localizacao = {}
-localizacao["Carregamento"] = [6, 0]
-localizacao["Home e Energia"] = [1, 4]
-localizacao["Estoque Intermediario"] = [6, 4]
+localizacao["Casa"] = [3,20]
+localizacao["Santo Amaro"] = [2,16]
+localizacao["Socorro"] = [2,14]
 
-localizacao["Deposito Insumos 1"] = [2, 8]
-localizacao["Deposito Insumos 2"] = [6, 8]
+localizacao["Pinheiros"] = [3,9]
+localizacao["República"] = [4,6]
 
-localizacao["Estoque Pronto"] = [6, 1]
+localizacao["Luz"] = [1,3]
 
-localizacao["Estacao 1"] = [4, 8]
-localizacao["Estacao 2"] = [7, 7]
-localizacao["Estacao 3"] = [7, 5]
-localizacao["Estacao 4"] = [4, 1]
+localizacao["Sé"] = [1,1]
+localizacao["Barra Funda"] = [4,1]
+localizacao["Impacta"] = [6,1]
